0ef73eed459a2a3c3f1f7e6b29d04c9f36662e18
Boris Kourtoukov <boriskourt@gmail.com>
2013-12-08 04:14:18 -0500

    Updating to latest, (which happens to be broken)

--- cljs/bkblog.cljs
+++ cljs/bkblog.cljs
@@ -11,6 +11,11 @@
 
 (def log #(.log js/console %))
 
+(defn in? 
+  "true if seq contains elm"
+  [seq elm]  
+  (some #(= elm %) seq))
+
 (def root-path (replace (replace js/window.location.href (str "http://" js/window.location.host) "") js/window.location.hash ""))
 
 (def viewer (str "viewer/web/viewer.html?zoom=pagewidth&file=" root-path "posts/"))
@@ -18,7 +23,12 @@
 (em/deftemplate article-frame :compiled "cljs/templates/frame.html" [pdf-path]
   ["iframe"] (ef/set-attr :src (str pdf-path ".pdf")))
 
-(em/deftemplate article-list "cljs/templates/navigation.html" [])
+(em/deftemplate article-list "cljs/templates/navigation.html" [type]
+  ["li"] (if 
+    (not= type :standard) 
+    (ef/do-> 
+      (ef/remove-attr :data-article)
+      (ef/set-attr :data-direct ""))))
 
 (defn set-window-hash-args [args-map]
   (let [hash-str (reduce (fn [old-str [k v]] (str old-str (name k) "=" v "&")) "#" args-map)
@@ -57,27 +67,72 @@
             (selected-article pdf)
             (set-hash pdf)))))))
 
+(let [direct-menu-clicks (aslisten-live "li[data-direct]" :mouseover)]
+  (go (while true
+    (let [target (<! direct-menu-clicks)
+          pdf (ef/from target (ef/get-attr :data-pdf))
+          content (ef/from target (ef/get-text))]
+        (ef/at target 
+          (ef/do->
+            (ef/html-content 
+              (str "<a href='" root-path "posts/" pdf ".pdf' target='_blank'>" content "</a>"))))))))
+
+(defn adjust-alt-menu []
+  (let [ pdf (ef/from target (ef/get-attr :data-pdf))
+         content (ef/from target (ef/get-text))]
+        (ef/at target 
+          (ef/do->
+            (ef/html-content 
+              (str "<a href='" root-path "posts/" pdf ".pdf' target='_blank'>" content "</a>"))))))))
+
+
 (defn init-pdf []
   (let [latest-pdf (ef/from "li[data-article]:first-child" (ef/get-attr :data-pdf))
         pdf-file (if (empty? (str js/window.location.hash)) latest-pdf (subs js/window.location.hash 6))
         pdf-path (str viewer pdf-file)]
+        (log latest-pdf)
         (set-hash pdf-file)
         (selected-article pdf-file)
         (ef/at ".right" 
           (ef/content (article-frame pdf-path)))))
 
-(defn setup [available]
-  (log available)
-  (ef/at ".blog-navigation" (ef/html-content (article-list)))
-  (ef/at ".bktran" (ef/html-content (dm/menu "Boris<span>Kourt</span>" "/blog/" {:cat "cat" :bat "bat"} {":cat" "http://hats.hats cat bat" ":bat" "http://hats.hats cat bat"})))
-  (set-mobile-menu)
-  (init-pdf)
-  (set-height))
+(defn css-rules [condition-value]
+  (case condition-value
+    (:standard) (log "All works")
+    (:lacks-flex) (log "Lacks Flex")
+    (:lacks-reader) (log "Lacks Rreader")))
+
+(defn setup 
+  "Available = [canvas,webwokers,flex]"
+  [available]
+  (case available
+    ([true true true]) (do 
+      (ef/at ".unsupported__menu" (ef/html-content (article-list :lacks-reader)))
+      (set-mobile-menu)
+      (css-rules :standard)
+      (init-pdf))
+    ([true true nil] [true nil nil]) (do 
+      (ef/at ".unsupported__menu" (ef/html-content (article-list :standard)))
+      (set-mobile-menu)
+      (css-rules :lacks-flex)
+      (init-pdf))
+    (do 
+      (ef/at ".unsupported__menu" (ef/html-content (article-list :lacks-reader)))
+      (css-rules :lacks-reader)))
+  (set-height)
+;;(ef/at ".bktran" (ef/html-content (dm/menu pull-menu)))
+)
 
 ;; (select-keys map keyseq)
-
 (defn modernity []
-  (let [available (ef/from "html" (ef/get-attr :class))]
-    (em/wait-for-load (setup [vec available]))))
+  (let [available (split (ef/from "html" (ef/get-attr :class)) " ")
+        canvas    (in? available "canvas")
+        webwork   (in? available "webworkers")
+        flex      (if (in? available "flexbox")
+                      true 
+                      (if (in? available "flexboxlegacy")
+                          true)
+                      nil)]
+    (em/wait-for-load (setup [canvas webwork flex]))))
 
 (modernity)
--- cljs/dm.cljs
+++ cljs/dm.cljs
@@ -21,10 +21,34 @@
 	[".fm__link"] (do 
 					(ef/set-attr :href a)
 					(ef/content a)))
+; (comment
+;   Keys should be:
+;   Logo - >  map name link
+;   Main - >  map name type
+;   Sub  - >  map name type link
+;   Social -> map name link
+;   Info - >  string description
 
-(em/deftemplate menu :compiled "cljs/templates/dm.html" [nom home core-links sub-links]
-	[".fm--logo .fm__link"] (ef/content nom) 
-	[".fm--logo .fm__link"] (ef/set-attr :href home)
-	[".fm--core ul"] (ef/html-content (map #(core-menu-item (core-links %)) (keys core-links)))
-	[".fm__subnav ul"] (ef/html-content (map #(sub-menu-item (sub-links %)) (keys sub-links))))
+;   contains?
+;   select-keys map keyseq
+; {:logo   {	:title "BorisKourt" :url "/" } 
+; :main   {	"Projects" {"A project" "#"
+; "A cat project" "#"} 
+; "Talking"  {"A rat talk" "#"
+; "Wombats" "#"}
+; "Other"    {"Rhombus" "#"}}
+; :social {}
+; :info 	 {}}
+;   )
+
+(em/deftemplate menu :compiled "cljs/templates/dm.html" [data]
+	(let [logo (select-keys data [:logo])]
+		(if (contains? :url logo)
+			[".fm--logo .fm__link"] (ef/content "hat")
+			[".fm--logo .fm__link"] (ef/set-attr :href "/")))
+	;[".fm--logo .fm__link"] (ef/content nom) 
+	;[".fm--logo .fm__link"] (ef/set-attr :href home)
+	;[".fm--core ul"] (ef/html-content (map #(core-menu-item (core-links %)) (keys core-links)))
+	;[".fm__subnav ul"] (ef/html-content (map #(sub-menu-item (sub-links %)) (keys sub-links)))
+	)
 
