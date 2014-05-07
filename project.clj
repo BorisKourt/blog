78b4e053a4932a517442f36560047988a262b608
Boris Kourtoukov <boriskourt@gmail.com>
2014-05-07 19:57:03 -0400

    /b/

--- project.clj
+++ project.clj
@@ -1,12 +1,13 @@
 (defproject bkblog "1"
   :description "UI for boriskourt.com"
   :url "http://boriskourt.com/"
-  :dependencies [[org.clojure/clojure "1.6.0-alpha2"]
-                 [org.clojure/clojurescript "0.0-2120"]
-                 [com.cemerick/url "0.1.0"]
-                 [enfocus "2.0.2"]
-                 [org.clojure/core.async "0.1.262.0-151b23-alpha"]
-                 [net.clojure/monads "1.0.0"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-2202"]
+                 [com.cemerick/url "0.1.1"]
+                 [kioo "0.4.1-SNAPSHOT"]
+                 [om "0.6.2"]
+                 [org.clojure/core.async "0.1.278.0-76b25b-alpha"]
+                 [secretary "1.1.0"]]
   :plugins [[lein-cljsbuild "1.0.0"]]
   :cljsbuild {
               :builds [{:source-paths ["cljs"]
